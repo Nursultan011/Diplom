89bf02903583430fda38a931fa32edebe113e029
Nursultan <nmukhamedali@gmail.com>
2023-06-26 17:24:11 +0600

    f

--- src/components/partials/Catalog/CatalogItem.vue
+++ src/components/partials/Catalog/CatalogItem.vue
@@ -1,101 +1,102 @@
 <template>
-    <div class="catalog-item">
-        <img src="../../../assets/catalog/dress1.png" alt="item.name" class="catalog-item__image">
-        <h4 class="catalog-item__name">{{ item.name }}</h4>
-        <p class="catalog-item__cost">{{ item.cost }} KZT</p>
+  <div class="catalog-item">
+    <img
+      src="../../../assets/catalog/dress1.png"
+      alt="item.name"
+      class="catalog-item__image"
+    />
+    <h4 class="catalog-item__name">{{ item.name }}</h4>
+    <p class="catalog-item__cost">{{ item.cost }} KZT</p>
 
-        <button class="catalog-item__btn">ADD TO BAG</button>
-        <button class="catalog-item__like"></button>
-    </div>
+    <button class="catalog-item__btn">ADD TO BAG</button>
+    <button class="catalog-item__like"></button>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'CatalogItem',
-    props: {
-        item: {
-            default: {},
-            type: Object
-        }
-    }
-}
+  name: "CatalogItem",
+  props: {
+    item: {
+      default: {},
+      type: Object,
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-    .catalog-item {
-        width: 285px;
-        height: 448px;
+.catalog-item {
+  width: 285px;
+  height: 448px;
 
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        padding: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 16px;
 
-        box-sizing: border-box;
+  box-sizing: border-box;
 
-        border: 1px solid var(--grey-2, #E1E5EC);
-        background: var(--white, #FFF);
+  border: 1px solid var(--grey-2, #e1e5ec);
+  background: var(--white, #fff);
 
-        position: relative;
+  position: relative;
 
-        &__image {
-            width: 238px;
-            height: 238px;
-        }
+  &__image {
+    width: 238px;
+    height: 238px;
+  }
 
-        &__name {
-            text-align: left;
-            color: var(--text-color, #000);
-            font-size: 16px;
-            font-family: Montserrat;
-            font-weight: 600;
+  &__name {
+    text-align: left;
+    color: var(--text-color, #000);
+    font-size: 16px;
+    font-family: Montserrat;
+    font-weight: 600;
 
-            width: 100%;
-        }
+    width: 100%;
+  }
 
-        &__cost {
-            text-align: left;
+  &__cost {
+    text-align: left;
 
-            color: var(--text-color, #000);
-            font-size: 18px;
-            font-family: Montserrat;
-            font-weight: 700;
+    color: var(--text-color, #000);
+    font-size: 18px;
+    font-family: Montserrat;
+    font-weight: 700;
 
-            width: 100%;
+    width: 100%;
+  }
 
-        }
+  &__btn {
+    display: flex;
+    padding: 10px 20px;
+    justify-content: center;
+    align-items: center;
 
-        &__btn {
-            display: flex;
-            padding: 10px 20px;
-            justify-content: center;
-            align-items: center;
+    border: 0.5px solid #000;
+    background: #fff;
 
-            border: 0.5px solid #000;
-            background: #FFF;
+    color: #000;
+    font-size: 16px;
+    font-family: Montserrat;
+    font-weight: 600;
 
+    align-self: flex-start;
+  }
 
-            color: #000;
-            font-size: 16px;
-            font-family: Montserrat;
-            font-weight: 600;
+  &__like {
+    width: 24px;
+    height: 24px;
+    background: url(../../../assets/catalog/heart.png);
+    background-size: contain;
 
-            align-self: flex-start;
+    border: none;
 
-        }
+    position: absolute;
 
-        &__like {
-            width: 24px;
-            height: 24px;
-            background: url(../../../assets/catalog/heart.png);
-            background-size: contain;
-
-            border: none;
-
-            position: absolute;
-
-            top: 32px;
-            right: 25px;
-        }
-    }
+    top: 32px;
+    right: 25px;
+  }
+}
 </style>
